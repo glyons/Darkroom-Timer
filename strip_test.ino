8b2d674879cc8ecfa3032d390c2141e1fd877c14
Gavin Lyons <admin@i386.com>
2023-07-21 11:48:23 +0200

    Strip Test Feature 1/2,1/3,1/6,1/12

--- strip_test.ino
+++ strip_test.ino
@@ -1,117 +1,87 @@
 // Strip Test
-float *stripValueTest(float t, byte mode, float values[],int stops[])
+void stripValueTest(float t, int mode, int values[],int stops[])
 {
-  int fullstops []= {0,100,200,300,400,500};
-  int halfstops []= {0,50,100,150,200,250};
-  int thirdstops []= {0,33,66,100,133,166};
-  
+  int full []= {0,100,200,300,400,500};
+  int halves []= {0,50,100,150,200,250};
+  int thirds []= {0,33,66,100,133,166};
+  int sixths []={0,16,33,50,66,83};
+  int twelveths [] {0,8,16,25,33,42};
+
+  values[0]=t;
   switch (mode)
   {
-      case 0: //Off
+     case 0:///Sixths strip test calculation
+       values[1]=values[0]+t/12;
+       values[2]=values[1]+t/12;
+       values[3]=values[2]+t/12;
+       values[4]=values[3]+t/12;
+       values[5]=values[4]+t/12;
+       for(int n=0;n<=5;n++)
+        stops[n]=thirds[n];
        break;
-      case 1://Half Stops strip test calculation
-       values[0]=t;
-       values[1]=values[0]+t/2;
-       values[2]=values[1]+t/2;
-       values[3]=values[2]+t;
-       values[4]=values[3]+t;
-       values[5]=values[4]+t*2;
-       for(int n=0;n++;n<6)
-       {
-        stops[n]=halfstops[n];
-       }
+     case 1:///Sixths strip test calculation
+       values[1]=values[0]+t/6;
+       values[2]=values[1]+t/6;
+       values[3]=values[2]+t/6;
+       values[4]=values[3]+t/6;
+       values[5]=values[4]+t/6;
+       for(int n=0;n<=5;n++)
+        stops[n]=thirds[n];
        break;
-      case 2:///Third Stops strip test calculation
-       values[0]=t;
+      case 2:///Thirds strip test calculation
        values[1]=values[0]+t/3;
        values[2]=values[1]+t/3;
        values[3]=values[2]+t/3;
        values[4]=values[3]+t/3*2;
        values[5]=values[4]+t/3*2;
-       for(int n=0;n++;n<6)
-       {
-        stops[n]=thirdstops[n];
-       }
+       for(int n=0;n<=5;n++)
+        stops[n]=thirds[n];
        break;
-      case 3://Full Stop strip test calculation
-       values[0]=t;
-       values[1]=values[0]+t;
-       values[2]=values[1]+t*2;
-       values[3]=values[2]+t*4;
-       values[4]=values[3]+t*8;
-       values[5]=values[4]+t*16;
-       for(int n=0;n++;n<6)
-       {
-        stops[n]=fullstops[n];
-       }
+      case 3://Halves strip test calculation
+       values[1]=values[0]+t/2;
+       values[2]=values[1]+t/2;
+       values[3]=values[2]+t;
+       values[4]=values[3]+t;
+       values[5]=values[4]+t*2;
+       for(int n=0;n<=5;n++)
+        stops[n]=halves[n];
+       break;
+       case 4://Full Stop strip test calculation
+         values[0]=t;
+         values[1]=values[0]+t;
+         values[2]=values[1]+t*2;
+         values[3]=values[2]+t*4;
+         values[4]=values[3]+t*8;
+         values[5]=values[4]+t*16;
+         for(int n=0;n<=5;n++)
+          stops[n]=full[n];
        break;
   }
-  Serial.println(values[0]);
-  Serial.println(values[1]);
-  Serial.println(values[2]);
-  Serial.println(values[3]);
-  Serial.println(values[4]);
-  Serial.println(values[5]);
-  Serial.println("------");
   return values;
 }
-void stripTestModeSetUp()//Program steps input mode set-up, long hold program button
-{
-  if (tmButtons==STRIPTEST_MODE_BUTTON) plusminus++;
-  stripTestMode = plusminus % 4;//read 2 revolving encoder values
-  if(stripTestMode != displayRefreshTracker) //check for display update
-  { 
-    switch (stripTestMode)
-    {
-      case 0:
-        tm.displayText("   Off  ");
-      break;
-      case 1:
-        tm.displayText("  HALF  ");
-      break;
-      case 2:
-        tm.displayText("  third ");
-      break;
-      case 3:
-        tm.displayText("  Full  ");
-       break;
-    }
-    EEPROM.write(eeStripTestMode, stripTestMode);
-  }
-  displayRefreshTracker = stripTestMode; //update of display update trigger
-  delay(intervalButton); 
-}
+
 void stripTest()//select strip number for stripBuilder(), simple click button 2
 {
-    switch(tmButtons)
-    {
-      case STRIPTEST_BUTTON:
-        plusminus++;
-      break;
-      case 1:
-       uiMode=0;
-      break;
-    }
-  delay(intervalButton);  
-  stripID =  plusminus % 6; 
   clearStripLEDs();
-  tm.setLED(stripID+1, 1);
-  if (stripID != displayRefreshTracker) // refresh display
+  int values[6]; int fstops[6];
+  stripValueTest(tensSeconds,stepIdx, values, fstops);        
+  for(int n=0;n<=5;n++)
   {
-    if (stripID==0)
+    tm.setLED(n+1,1);
+    sprintf(tempString,  " %03d%4d",fstops[n], values[n]);
+    displayText(tempString,1,6);
+    if (!focusLight)
     {
-      sprintf(tempString, "bASE%4d", tensSeconds);
-      displayText(tempString,99,6);
+      FStop=fstops[n];
+      timerCountdown(values[n]);//timer with f-stop converted in time + enlargment correction time
+      if (FStop < 1000) sprintf(tempString, " %03d%4d", FStop, 0); //fstop format rule 
+      if (FStop >= 1000) sprintf(tempString, "%4d%4d", FStop, 0); //fstop format rule
+      displayText(tempString,1,99);
     }
-    else
-    {
-      float values[6]; int stops[6];
-      stripValueTest(tensSeconds, stripTestMode, values, stops);
-      sprintf(tempString," %03d    ", stops[stripID]);//values[stripID]
-      displayText(tempString,1,6);
-      Serial.println(stops[stripID]);
-       Serial.println(values[stripID]);
-    }
+    displayRefreshTracker += 1;
+    delay(1000);
   }
- displayRefreshTracker = stripID; //value for display update check
+  FStop=fstops[0];
+  clearStripLEDs();
+  tm.setLED(stepIdx, 1); // restore LED to step state
 }
