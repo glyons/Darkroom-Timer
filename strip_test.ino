b5ec4ed618bdb0e70ea7fa66cedb00e749e384b9
Gavin Lyons <admin@i386.com>
2023-07-28 19:55:21 +0200

    Strip Test improvements

--- strip_test.ino
+++ strip_test.ino
@@ -1,5 +1,5 @@
 // Strip Test
-void stripValueTest(float t, int mode, int values[],int stops[])
+void stripValueTest(float t, int mode, int values[],int stops[], int exposeValues[6])
 {
   int full []= {0,100,200,300,400,500};
   int halves []= {0,50,100,150,200,250};
@@ -17,7 +17,10 @@ void stripValueTest(float t, int mode, int values[],int stops[])
        values[4]=values[3]+t/12;
        values[5]=values[4]+t/12;
        for(int n=0;n<=5;n++)
+       {
         stops[n]=thirds[n];
+        exposeValues[n]=t/12;
+       }
        break;
      case 1:///Sixths strip test calculation
        values[1]=values[0]+t/6;
@@ -26,7 +29,10 @@ void stripValueTest(float t, int mode, int values[],int stops[])
        values[4]=values[3]+t/6;
        values[5]=values[4]+t/6;
        for(int n=0;n<=5;n++)
+       {
         stops[n]=thirds[n];
+        exposeValues[n]=t/6;
+       }
        break;
       case 2:///Thirds strip test calculation
        values[1]=values[0]+t/3;
@@ -35,7 +41,10 @@ void stripValueTest(float t, int mode, int values[],int stops[])
        values[4]=values[3]+t/3*2;
        values[5]=values[4]+t/3*2;
        for(int n=0;n<=5;n++)
+      {
         stops[n]=thirds[n];
+        exposeValues[n]=t/3;
+       }
        break;
       case 3://Halves strip test calculation
        values[1]=values[0]+t/2;
@@ -44,43 +53,60 @@ void stripValueTest(float t, int mode, int values[],int stops[])
        values[4]=values[3]+t;
        values[5]=values[4]+t*2;
        for(int n=0;n<=5;n++)
+       {
         stops[n]=halves[n];
+        exposeValues[n]=t/2;
+       }
        break;
        case 4://Full Stop strip test calculation
-         values[0]=t;
          values[1]=values[0]+t;
          values[2]=values[1]+t*2;
          values[3]=values[2]+t*4;
          values[4]=values[3]+t*8;
          values[5]=values[4]+t*16;
          for(int n=0;n<=5;n++)
-          stops[n]=full[n];
+         {
+            stops[n]=full[n];
+            exposeValues[n]=t;
+         }
        break;
+       exposeValues[0]=t; // Base time
   }
-  return values;
 }
 
 void stripTest()//select strip number for stripBuilder(), simple click button 2
 {
   clearStripLEDs();
-  int values[6]; int fstops[6];
-  stripValueTest(tensSeconds,stepIdx, values, fstops);        
+  int values[6]; int fstops[6]; int exposeValues[6];
+  stripValueTest(tensSeconds,stepIdx, values, fstops,exposeValues);   
+  stripTestMode=true;     
   for(int n=0;n<=5;n++)
   {
     tm.setLED(n+1,1);
-    sprintf(tempString,  " %03d%4d",fstops[n], values[n]);
-    displayText(tempString,1,6);
+    if (n==0)
+    {
+       sprintf(tempString,"BASE%4d",values[n]);
+       displayText(tempString,99,6);
+    }
+    else
+    {
+       sprintf(tempString,  " %03d%4d",fstops[n], values[n]);
+       displayText(tempString,1,6);
+    }
+    
     if (!focusLight)
     {
       FStop=fstops[n];
-      timerCountdown(values[n]);//timer with f-stop converted in time + enlargment correction time
-      if (FStop < 1000) sprintf(tempString, " %03d%4d", FStop, 0); //fstop format rule 
-      if (FStop >= 1000) sprintf(tempString, "%4d%4d", FStop, 0); //fstop format rule
+      timerCountdown(exposeValues[n]);//timer with f-stop converted in time + enlargment correction time
+      if (FStop < 1000) sprintf(tempString, " %03d End", FStop); //fstop format rule 
+      if (FStop >= 1000) sprintf(tempString, "%4d End", FStop); //fstop format rule
       displayText(tempString,1,99);
     }
     displayRefreshTracker += 1;
+    if (!stripTestMode) break;
     delay(1000);
   }
+  stripTestMode=false;
   FStop=fstops[0];
   clearStripLEDs();
   tm.setLED(stepIdx, 1); // restore LED to step state
