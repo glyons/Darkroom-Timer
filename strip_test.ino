62cefa72fbf634a07e8744e6a3e49709e50e0d5a
Gavin Lyons <admin@i386.com>
2023-08-11 07:47:34 +0200

    Revert some changes for Contiune AI

--- strip_test.ino
+++ strip_test.ino
@@ -49,21 +49,43 @@ void stripTest()//select strip number for stripBuilder(), simple click button 2
 {
   clearStripLEDs();
   int values[6]; int fstops[6]; int exposeValues[6];
-  stripValueTest(tensSeconds,stepIdx, values, fstops,exposeValues);
+  stripValueTest(tensSeconds,stepIdx, values, fstops,exposeValues);   
   stripTestMode=true;
-  int d=6;
+  int d=6;     
   for(int n=0;n<=5;n++)
   {
     tm.setLED(n+1,1);
-    sprintf(tempString,(n==0) ? "BASE%4d" : (values[n] < 10000) ? " %03d%4d" : "%4d",values[n]);
-    displayText(tempString, (n==0 || values[n] >= 10000) ? 99 : 1, d);
-    baseExposure = (n==0);
+    if (n==0)
+    {
+       sprintf(tempString,"BASE%4d",values[n]);
+       displayText(tempString,99,6);
+       baseExposure=true;
+    }
+    else
+    {
+      if (values[n] < 10000) d=6;
+      if (values[n] >= 10000) d=99;
+      sprintf(tempString,  " %03d%4d",fstops[n], values[n]);
+      displayText(tempString,1,d);
+      baseExposure=false;
+    }
+    
     if (!focusLight)
     {
       FStop=fstops[n];
       timerCountdown(exposeValues[n]);//timer with f-stop converted in time + enlargment correction time
-      sprintf(tempString, (FStop < 1000) ? " %03d End" : "%4d End", FStop); //fstop format rule
-      displayText(tempString,1,99);
+      if (FStop < 1000) 
+      {
+        sprintf(tempString, " %03d End", FStop); //fstop format rule 
+        if (n==0) displayText("BASE End",99,99); 
+          else displayText(tempString,1,99);
+      }
+      if (FStop >= 1000) 
+      {
+        sprintf(tempString, "%4d End", FStop); //fstop format rule
+        displayText(tempString,99,99);
+      }
+      
     }
     displayRefreshTracker += 1;
     if (!stripTestMode) break;
